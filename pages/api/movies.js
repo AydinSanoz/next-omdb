73bb75c50e46a02f032bd838dcdda79497a93a63
E2029-Aydin <aydinsanoz2@gmail.com>
2021-10-30 12:08:28 +0300

    search cookie set & swr option added

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,12 +1,15 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export default (req, res) => {
 	const { BASEURL, APIKEY } = process.env;
+
 	axios
 		.get(BASEURL, {
 			params: {
 				apikey: APIKEY,
-				s: "avatar",
+				s: req.cookies.search || "avatar",
+				type: "movie",
 			},
 		})
 		.then(({ data }) => res.status(200).json(data.Search));
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,32 @@ import Card from "../components/card";
 
 function HomePage(props) {
 	const url = "http://localhost:3000/api/movies";
-	const { data, error } = useSWR(url, fetcher);
+	console.log("Indexprops", props);
+
+	const { data, error } = useSWR(url, fetcher, {
+		initialData: props,
+		revalidateOnMount: true,
+		revalidateOnFocus: true,
+	});
+
 	if (error) return <div>error occured</div>;
 	if (!data) return <div>loading</div>;
 	console.log(data);
 
 	return (
 		<div>
-			{data.map((item, i) => (
-				<Card key={i} {...item} />
-			))}
+			<div
+				className="row"
+				style={{
+					overflowX: "auto",
+					overflowY: "hidden",
+					flexWrap: "nowrap",
+				}}
+			>
+				{data.map((item, i) => (
+					<Card key={i} {...item} />
+				))}
+			</div>
 		</div>
 	);
 }
