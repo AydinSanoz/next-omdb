278b8e3bd1545513cf61d130d2a97c08ec9d82be
E2029-Aydin <aydinsanoz2@gmail.com>
2021-10-31 22:48:40 +0300

    bug: cors policy

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
-// module.exports = {
-// 	reactStrictMode: true,
-// 	async rewrites() {
-// 		return [
-// 			{
-// 				source: "/api/:path*",
-// 				destination: "http:///www.omdbapi.com/:path*",
-// 			},
-// 		];
-// 	},
-// };
-
-// next.config.js
 module.exports = {
-	async headers() {
+	reactStrictMode: true,
+	async rewrites() {
 		return [
 			{
-				// matching all API routes
 				source: "/api/:path*",
+				destination: "http://www.omdbapi.com/:path*",
 				headers: [
 					{ key: "Access-Control-Allow-Credentials", value: "true" },
 					{ key: "Access-Control-Allow-Origin", value: "*" },
@@ -34,3 +22,29 @@ module.exports = {
 		];
 	},
 };
+
+// next.config.js
+// module.exports = {
+// 	async headers() {
+// 		return [
+// 			{
+// 				// matching all API routes
+// 				source: "/api/:path*",
+// 				headers: [
+// 					{ key: "Access-Control-Allow-Credentials", value: "true" },
+// 					{ key: "Access-Control-Allow-Origin", value: "*" },
+// 					{
+// 						key: "Access-Control-Allow-Methods",
+// 						value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+// 					},
+// 					{
+// 						key: "Access-Control-Allow-Headers",
+// 						value:
+// 							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+// 					},
+// 				],
+
+// 			},
+// 		];
+// 	},
+// };
