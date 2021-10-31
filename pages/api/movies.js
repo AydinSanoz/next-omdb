dbf15f258a6069a4f9d16d422a750f005d4b6888
Aydin <65701440+AydinSanoz@users.noreply.github.com>
2021-11-01 01:03:15 +0300

    Update movies.js

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -4,16 +4,12 @@ import Cookies from "js-cookie";
 export default (req, res) => {
 	const { BASEURL, APIKEY } = process.env;
 
-	axios
-		.get(BASEURL, {
-			method:"HEAD",
-			mode:"no-cors",
-			params: {
-				apikey: APIKEY,
-				s: req.cookies.search || "all",
-				type: "movie",
-			},
+	const res = fetch("http://www.omdbapi.com/?apikey=10a28510&s=avatar", {
+		method: "HEAD",
+		mode: "no-cors",
+	});
+	const data = res.json()
 		})
-		.then(({ data }) => res.status(200).json(data.Search))
-		.catch((err) => res.status(400).json({ error: err }));
+		res.status(200).json(data.Search))
+		
 };
